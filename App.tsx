a4220df6f80b99c48d69853b5a083a3df1f8f1d4
Somesh Chandimal <someshchandih@gmail.com>
2023-09-02 05:28:42 +0530

    add UserContext and CartContext

--- App.tsx
+++ App.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeNavigator from "./src/navigation/HomeNavigator";
 
+import UserContext from "./src/context/UserContext";
+import CartContext from "./src/context/CartContext";
+
+import { getEmployees } from "./src/services/fakeEmployeeService";
+
 export default function App() {
+  // const [user, setUser] = React.useState(null);
+  const [user, setUser] = React.useState(
+    getEmployees().find((c) => c.id === 1)
+  );
+  const [cart, setCart] = React.useState([]);
+
   return (
-    <NavigationContainer>
-      <HomeNavigator />
-    </NavigationContainer>
+    <UserContext.Provider value={{ user, setUser }}>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <NavigationContainer>
+          <HomeNavigator />
+        </NavigationContainer>
+      </CartContext.Provider>
+    </UserContext.Provider>
   );
 }
