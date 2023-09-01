b1fa375aa97a8b428724a832c27658e61b50070f
Somesh Chandimal <someshchandih@gmail.com>
2023-09-02 05:27:44 +0530

    add PopUpModel

---
+++ src/components/PopUpModal.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, StyleSheet, Modal } from "react-native";
+
+function PopUpModal({ modalVisible, setModalVisible, children }) {
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>{children}</View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  modalView: {
+    borderColor: "black",
+    borderWidth: 2,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+});
+
+export default PopUpModal;
